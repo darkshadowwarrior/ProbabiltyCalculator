490ab2c5d5809c5a194db26f8a188490e0b26e2a
darkshadowwarrior <ianrichards@puresoftwaredesign.co.uk>
2023-05-16 13:48:59 +0100

    Refactored ProbabilityProcessor

--- Calculator.Core.Tests/Processor/ProbabilityProcessor.cs
+++ Calculator.Core.Tests/Processor/ProbabilityProcessor.cs
@@ -8,12 +8,17 @@
 
         public double CombinedWith(double p1, double p2)
         {
-            if(p1 is < 0 or > 1 || p2 is < 0 or > 1)
+            ThrowExceptionIfArguementsOutOfRange(p1, p2);
+
+            return p1 * p2;
+        }
+
+        private static void ThrowExceptionIfArguementsOutOfRange(double p1, double p2)
+        {
+            if (p1 is < 0 or > 1 || p2 is < 0 or > 1)
             {
                 throw new ArgumentOutOfRangeException();
             }
-
-            return p1 * p2;
         }
     }
 }
