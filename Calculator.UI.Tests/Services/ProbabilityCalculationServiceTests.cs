f651ca1a14579c29cec25ceb683c12393fffa14b
darkshadowwarrior <ianrichards@puresoftwaredesign.co.uk>
2023-05-16 20:17:31 +0100

    Added WhenCalculateIsCalled_ShouldCallEither_AndReturnResponse failing test

--- Calculator.UI.Tests/Services/ProbabilityCalculationServiceTests.cs
+++ Calculator.UI.Tests/Services/ProbabilityCalculationServiceTests.cs
@@ -24,5 +24,18 @@ namespace Calculator.UI.Services
 
             Assert.Equal(0.25, response.Result);
         }
+
+        [Fact]
+        public void WhenCalculateIsCalled_ShouldCallEither_AndReturnResponse()
+        {
+            var service = new ProbabilityCalculationService(_processorMock.Object);
+            _processorMock.Setup(x => x.Either(It.IsAny<double>(), It.IsAny<double>())).Returns(0.75);
+
+            ProbabilityCalculationResponse response = service.Calculate(new ProbabilityCalculationRequest { ProbabilityA = 0.5, ProbabilityB = 0.5, TypeOfCalculation = "CombinedWith" });
+
+            _processorMock.Verify(x => x.Either(It.IsAny<double>(), It.IsAny<double>()));
+
+            Assert.Equal(0.25, response.Result);
+        }
     }
 }
