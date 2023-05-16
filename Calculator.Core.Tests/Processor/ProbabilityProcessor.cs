f48a4d17c2b7a280cf2201a7aafb55a50202e9c2
darkshadowwarrior <ianrichards@puresoftwaredesign.co.uk>
2023-05-16 13:51:23 +0100

    Fixed failing test

--- Calculator.Core.Tests/Processor/ProbabilityProcessor.cs
+++ Calculator.Core.Tests/Processor/ProbabilityProcessor.cs
@@ -13,17 +13,20 @@
             return p1 * p2;
         }
 
+        public double Either(double p1, double p2)
+        {
+            var a = p1 + p2;
+            var b = p2 * p1;
+
+            return a - b;
+        }
+
         private static void ThrowExceptionIfArguementsOutOfRange(double p1, double p2)
         {
             if (p1 is < 0 or > 1 || p2 is < 0 or > 1)
             {
                 throw new ArgumentOutOfRangeException();
             }
-        }
-
-        internal object Either(double p1, double p2)
-        {
-            throw new NotImplementedException();
         }
     }
 }
