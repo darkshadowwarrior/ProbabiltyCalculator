b9ebab72e8a32fa41d1e715465b470720cea7377
darkshadowwarrior <ianrichards@puresoftwaredesign.co.uk>
2023-05-16 13:34:43 +0100

    Added new passing test case

--- Calculator.Core.Tests/Processor/ProbabilityProcessorTests.cs
+++ Calculator.Core.Tests/Processor/ProbabilityProcessorTests.cs
@@ -4,6 +4,7 @@
     {
         [Theory]
         [InlineData(0.5, 0.5, 0.25)]
+        [InlineData(0.3, 0.7, 0.21)]
         public void ShouldReturnCorrectProbability_WhenCombinedWithIsCalled(double p1, double p2, double expected)
         {
             var processor = new ProbabilityProcessor();
