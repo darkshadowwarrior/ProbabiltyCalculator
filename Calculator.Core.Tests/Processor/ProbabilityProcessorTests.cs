45b6baaea06b06fa2f7350a27269ec8e18eff207
darkshadowwarrior <ianrichards@puresoftwaredesign.co.uk>
2023-05-16 13:31:43 +0100

    Initial commit

---
+++ Calculator.Core.Tests/Processor/ProbabilityProcessorTests.cs
@@ -0,0 +1,12 @@
+﻿namespace Calculator.Core.Processor
+{
+    public class ProbabilityProcessorTests
+    {
+        [Theory]
+        [InlineData(0.5, 0.5, 0.25)]
+        public void ShouldReturnCorrectProbability_WhenCombinedWithIsCalled(double p1, double p2, double expected)
+        {
+
+        }
+    }
+}
