99613118d597083cbb2f778889aea8897ae7e0a3
darkshadowwarrior <ianrichards@puresoftwaredesign.co.uk>
2023-05-16 13:34:07 +0100

    Fixed breaking test

---
+++ Calculator.Core.Tests/Processor/ProbabilityProcessor.cs
@@ -0,0 +1,14 @@
+﻿namespace Calculator.Core.Processor
+{
+    public class ProbabilityProcessor
+    {
+        public ProbabilityProcessor()
+        {
+        }
+
+        public double CombinedWith(double p1, double p2)
+        {
+            return p1 * p2;
+        }
+    }
+}
