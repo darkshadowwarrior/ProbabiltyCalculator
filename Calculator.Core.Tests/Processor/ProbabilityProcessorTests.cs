c2e37990720f451dfc31f31cdce18c825a223c22
darkshadowwarrior <ianrichards@puresoftwaredesign.co.uk>
2023-05-16 13:33:02 +0100

    Introduced breaking test for CombinedWith

--- Calculator.Core.Tests/Processor/ProbabilityProcessorTests.cs
+++ Calculator.Core.Tests/Processor/ProbabilityProcessorTests.cs
@@ -6,6 +6,11 @@
         [InlineData(0.5, 0.5, 0.25)]
         public void ShouldReturnCorrectProbability_WhenCombinedWithIsCalled(double p1, double p2, double expected)
         {
+            var processor = new ProbabilityProcessor();
+
+            var result = processor.CombinedWith(p1, p2);
+
+            Assert.Equal(expected, result);
 
         }
     }
