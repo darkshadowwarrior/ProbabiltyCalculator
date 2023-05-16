f5ac8e385ab6d2d52eef5417ecb89071e21aefc6
darkshadowwarrior <ianrichards@puresoftwaredesign.co.uk>
2023-05-16 14:48:50 +0100

    Fixed failing test and bug

--- Calculator.Core/Data/ProbabilityRepository.cs
+++ Calculator.Core/Data/ProbabilityRepository.cs
@@ -15,7 +15,7 @@ namespace Calculator.Core.Repository
 
         public void Save(Probability probability)
         {
-            var line = $"Date calculation processed is: {probability.Date}, Type of calculation: {probability.TypeOfCalculation}, Result of calculation: {probability.Value}, Inputs used are: P1 = {probability.Inputs[0]} and P2 = {probability.Inputs[0]}";
+            var line = $"Date calculation processed is: {probability.Date}, Type of calculation: {probability.TypeOfCalculation}, Result of calculation: {probability.Value}, Inputs used are: P1 = {probability.Inputs[0]} and P2 = {probability.Inputs[1]}";
 
             _fileWriter.SaveToFile(line);
         }
